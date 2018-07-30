848935a95931b62304151b10145b353968504c0b
Jon Soza <jaysoza@gmail.com>
2018-07-30 10:24:03 -0400

    More files to be added to learning repository.

--- ex8.py
+++ ex8.py
@@ -1,4 +1,4 @@
-formatter = "%r %r %r"
+formatter = "%r %r %r %r"
 
 print formatter % (1, 2, 3, 4)
 print formatter % ("one", "two", "three", "four")
@@ -10,3 +10,6 @@ print formatter % (
 	"But it didn't sing.",
 	"So I said goodnight."
 )
+
+# 1. I did not enter the correct number of %r characters in the beginning
+# 2. The third line contains a single quote character used as an apostrophe; if removed, the output would match the other 3 lines in the poem. Since the single quote character exists in the string, double quotes are used around the entire line output to signal the line is part of a string.
